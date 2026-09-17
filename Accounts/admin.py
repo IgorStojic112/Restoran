2962da10ac5338f0227e519d480df9175ef407fb
markosalai <marko.salai@fsre.sum.ba>
2026-09-17 22:57:26 +0200

    admin moze vidjeti sve modele, ukljucujuci polja User modela

--- Accounts/admin.py
+++ Accounts/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
 
-admin.site.register(User, UserAdmin)
+# admin.site.register(User, UserAdmin)
+try:
+    admin.site.unregister(User)
+except admin.sites.NotRegistered:
+    pass
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ("Preferences & Profile", {
+            "fields": ("role", "profile_image", "dietary_preferences", "allergies")
+        }),
+    )
+    add_fieldsets = BaseUserAdmin.add_fieldsets + (
+        ("Role", {
+            "fields": ("role",)
+        }),
+    )
+    list_display = ["username", "email", "role", "is_staff", "is_active"]
+    list_filter = ["role", "is_staff", "is_active"]
+    search_fields = ["username", "email"]
+    filter_horizontal = BaseUserAdmin.filter_horizontal + ("allergies",)
--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from .models import Category, Ingredient, MeniItem, Order, OrderItem
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["id", "Name", "Description"]
+    search_fields = ["Name"]
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ["id", "Name", "is_allergen", "is_vegetarian", "is_vegan"]
+    list_filter = ["is_allergen", "is_vegetarian", "is_vegan"]
+    search_fields = ["Name"]
+
+
+@admin.register(MeniItem)
+class MeniItemAdmin(admin.ModelAdmin):
+    list_display = ["id", "Name", "Category", "Price", "Available"]
+    list_filter = ["Category", "Available"]
+    search_fields = ["Name"]
+    filter_horizontal = ["Ingredient"]
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+    readonly_fields = ["menu_item", "quantity", "price"]
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["id", "user", "status", "total_price", "created_at"]
+    list_filter = ["status"]
+    search_fields = ["user__username"]
+    inlines = [OrderItemInline]
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ["id", "order", "menu_item", "quantity", "price"]
